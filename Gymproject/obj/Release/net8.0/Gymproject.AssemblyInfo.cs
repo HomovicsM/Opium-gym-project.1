b4bcdc2290898925b765a5f6dc26f1ba42112a96
HomovicsM <homovicsm@kkszki.hu>
2025-03-28 10:21:59 +0100

    solution fajl csinalva mert eltunt az elozo

--- Gymproject/obj/Release/net8.0/Gymproject.AssemblyInfo.cs
+++ Gymproject/obj/Release/net8.0/Gymproject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gymproject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15a49295b5b2802788c4043bb66e624eac9f2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymproject")]
