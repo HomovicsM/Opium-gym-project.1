888d09b6d5e58019909fb7747655023533ee64cc
HomovicsM <93767210+HomovicsM@users.noreply.github.com>
2025-03-31 13:19:54 +0200

    Add files via upload

--- merchdb.sql
+++ merchdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Már 17. 13:09
+-- Létrehozás ideje: 2025. Már 31. 13:15
 -- Kiszolgáló verziója: 10.4.20-MariaDB
 -- PHP verzió: 7.3.29
 
@@ -24,16 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `products`
+-- Tábla szerkezet ehhez a táblához `product`
 --
 
-CREATE TABLE `products` (
+CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `image_url`) VALUES
+(1, 'teszt', 'teszt', '1000.00', 'https://st2.depositphotos.com/3765753/5349/v/950/depositphotos_53491489-stock-illustration-example-rubber-stamp-vector-over.jpg');
 
 -- --------------------------------------------------------
 
@@ -43,20 +50,20 @@ CREATE TABLE `products` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `is_admin` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- Indexek a kiírt táblákhoz
 --
 
 --
--- A tábla indexei `products`
+-- A tábla indexei `product`
 --
-ALTER TABLE `products`
+ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -71,10 +78,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT a táblához `products`
+-- AUTO_INCREMENT a táblához `product`
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT a táblához `users`
